d00688177ba85d2cba9aa63588ae370c7f32daa4
Luã Bida Vacaro <luabidaa@gmail.com>
2024-01-08 14:08:43 -0300

    chore(python-package): include release.yaml to on PYPI and semantic-release (#4)

--- src/mosqlient/__init__.py
+++ src/mosqlient/__init__.py
@@ -3,7 +3,17 @@ Mosqlimate client package.
 
 client library for the Mosqlimate project data platform.
 """
-
+from importlib import metadata as importlib_metadata
 from typing import List
 
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "0.1.0"  # changed by semantic-release
+
+
+version: str = get_version()
+__version__: str = version
 __all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
