293c4e1239fb93ffb1306c4c1b35c310ce9008d2
Luã Bida Vacaro <luabidaa@gmail.com>
2024-05-13 15:43:01 -0300

    Include async get_all generic method

--- mosqlient/config.py
+++ mosqlient/config.py
@@ -1,2 +1,3 @@
 APPS: list[str] = ["registry", "datastore", "vis"]
-API_BASE_URL = "https://api.mosqlimate.org/api/"
+# API_BASE_URL = "https://api.mosqlimate.org/api/"
+API_BASE_URL = "http://0.0.0.0:8042/api/"
--- mosqlient/requests.py
+++ mosqlient/requests.py
@@ -30,27 +30,31 @@ def get(
     return requests.get(base_url, params, timeout=timeout)
 
 
-async def _aget(
+async def aget(
     session: aiohttp.ClientSession,
     url: str,
     params: dict[str, str | int | float],
     timeout: int = 10,
     retries: int = 3
-):
+) -> dict:
     try:
         if retries < 0:
             raise aiohttp.ClientConnectionError("Too many attempts")
         async with session.get(url, params=params, timeout=timeout) as res:
             if res.status == 200:
                 return await res.json()
+            if retries == 0:
+                raise aiohttp.ClientConnectionError(
+                    f"Response status: {res.status}. Reason: {res.reason}"
+                )
             asyncio.sleep(10/(retries + 1))
-            await _aget(session, url, params, timeout, retries - 1)
-    except aiohttp.TimeoutError:
-        asyncio.sleep(10/(retries + 1))
-        await _aget(session, url, params, timeout, retries - 1)
+            await aget(session, url, params, timeout, retries - 1)
+    except aiohttp.ServerTimeoutError:
+        asyncio.sleep(8/(retries + 1))
+        await aget(session, url, params, timeout, retries - 1)
 
 
-def get_all(
+async def get_all(
     app: Literal[APPS],
     endpoint: AnyStr,
     params: dict[str, str | int | float],
@@ -59,11 +63,22 @@ def get_all(
 ) -> dict:
     params["page"] = 1
     params["per_page"] = _max_per_page
-    res = get(app, endpoint, params)
 
-    if res.status_code != 200:
-        raise requests.exceptions.HTTPError(
-            f"{res.url} returned {res.status_code}"
-        )
+    url = urljoin(API_BASE_URL, "/".join((app, endpoint))) + "/?"
 
-    total_pages = res.json()['pagination']['total_pages']
+    async with aiohttp.ClientSession() as session:
+        first_page = await aget(session, url, params)
+
+    total_pages = first_page['pagination']['total_pages']
+
+    if total_pages == 1:
+        return first_page['items']
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for page in range(2, total_pages + 1):
+            params["page"] = page
+            task = asyncio.create_task(aget(session, url, params)['items'])
+            tasks.append(task)
+        results = await asyncio.gather(*tasks)
+    return results
