5d2e22ed191f886cc4eeac9515a12560870e972b
Eduardo Correa Araujo <65051489+eduardocorrearaujo@users.noreply.github.com>
2024-05-27 23:44:56 -0300

    Add functions to get the data from the datastore

--- mosqlient/datastore/api.py
+++ mosqlient/datastore/api.py
@@ -1,18 +1,15 @@
-from datetime import datetime
+import pandas as pd 
 from typing import Any
-
-import pandas as pd
-
+from datetime import datetime
 from mosqlient.requests import get_datastore
 
 
-def validate_date(date_str: str, par_name: str) -> None:
+def validate_date(date_str: str, par_name:str) -> None:
     try:
         # Validate the date format
-        valid_date = datetime.strptime(date_str, "%Y-%m-%d")
+        valid_date = datetime.strptime(date_str, '%Y-%m-%d')
     except ValueError:
         raise ValueError(f"Invalid date format for '{par_name}', should be YYYY-mm-dd")
-
 
 def _params(**kwargs) -> dict[str, Any]:
     params = {}
@@ -33,9 +30,11 @@ class Infodengue:
         cls._validate_fields(**kwargs)
         params = _params(**kwargs)
 
-        gen_data = get_datastore("datastore", "infodengue", params)
+        gen_data = get_datastore('datastore', 'infodengue',
+        params)
 
         return pd.DataFrame(list(gen_data))
+    
 
     @staticmethod
     def _validate_fields(**kwargs) -> None:
@@ -48,9 +47,11 @@ class Copernicus:
         cls._validate_fields(**kwargs)
         params = _params(**kwargs)
 
-        gen_data = get_datastore("datastore", "climate", params)
+        gen_data = get_datastore('datastore', 'climate',
+        params)
 
         return pd.DataFrame(list(gen_data))
+    
 
     @staticmethod
     def _validate_fields(**kwargs) -> None:
@@ -63,8 +64,8 @@ class DataFieldValidator:
         "disease": str,
         "start": str,
         "end": str,
-        "uf": str,
-        "geocode": int,
+        "uf": str, 
+        "geocode": int, 
     }
     DISEASES = ["dengue", "zika", "chikungunya"]
 
@@ -74,22 +75,42 @@ class DataFieldValidator:
                 continue
 
             if not isinstance(v, self.FIELDS[k]):
-                raise TypeError(f"Field '{k}' must have instance of " f"{' or '.join(self.FIELDS[k])}")
+                raise TypeError(
+                    f"Field '{k}' must have instance of "
+                    f"{' or '.join(self.FIELDS[k])}"
+                )
 
             if k == "disease":
                 if v == "chik":
                     v = "chikungunya"
 
                 if v not in self.DISEASES:
-                    raise ValueError(f"Unkown 'disease'. Options: {self.DISEASES}")
-
-            if (k == "start") | (k == "end"):
+                    raise ValueError(
+                        f"Unkown 'disease'. Options: {self.DISEASES}"
+                    )
+                
+            if (k == 'start') | (k == 'end'):
                 validate_date(v, k)
 
-            if k == "uf":
-                if len(v) != 2:
-                    raise ValueError("Invalid 'uf' parameter. It should be a two-letter value")
 
-            if k == "geocode":
-                if len(str(v)) != 7:
-                    raise ValueError("Invalid 'geocode' parameter. It should be a seven numbers code")
+            if k == 'uf': 
+                if len(v) != 2: 
+                    raise ValueError(
+                        f"Invalid 'uf' parameter. It should be a two-letter value"
+                    )
+                
+            if k == 'geocode': 
+                if len(str(v)) != 7: 
+                    raise ValueError(
+                        f"Invalid 'geocode' parameter. It should be a seven numbers code"
+                    )
+                        
+                
+
+
+                
+
+                
+
+
+
