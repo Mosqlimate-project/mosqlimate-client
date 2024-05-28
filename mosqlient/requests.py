c4fb5d592e075c65479fc0c9f8f0882f748608ab
Eduardo Correa Araujo <65051489+eduardocorrearaujo@users.noreply.github.com>
2024-05-27 23:44:56 -0300

    Add functions to get the data from the datastore

--- mosqlient/requests.py
+++ mosqlient/requests.py
@@ -1,10 +1,11 @@
+import time
+import aiohttp
 import asyncio
 import requests
+import concurrent.futures
 from urllib.parse import urljoin
 from typing import AnyStr, Literal
 from itertools import chain
-
-import aiohttp
 
 from mosqlient.config import API_DEV_URL, API_PROD_URL, APPS, APPS_TYPE
 from mosqlient.utils import validate
@@ -96,3 +97,65 @@ async def get_all(
             tasks.append(task)
         results = await asyncio.gather(*tasks)
     return list(chain(result['items'] for result in results))
+
+
+
+def compose_url(base_url: str, parameters: dict, page: int = 1) -> str:
+    """Helper method to compose the API url with parameters"""
+    url = base_url + "?" if not base_url.endswith("?") else base_url
+    params = "&".join([f"{p}={v}" for p,v in parameters.items()]) + f"&page={page}"
+    return url + params
+
+
+def fetch_data(session: requests.Session, url: str):
+    """Uses ClientSession to create the async call to the API"""
+    response =  session.get(url)
+    return response.json()
+
+def attempt_delay(session: requests.Session, url: str):
+    """The request may fail. This method adds a delay to the failing requests"""
+    try:
+        return fetch_data(session, url)
+    except Exception as e:
+        time.sleep(0.2)
+        return attempt_delay(session, url)
+    
+
+def get_datastore(
+    app: APPS_TYPE,
+    endpoint: AnyStr,
+    params: dict[str, str | int | float],
+    _env: Literal["dev", "prod"] = "prod"
+) -> list[dict]:
+
+    base_url = API_DEV_URL if _env == "dev" else API_PROD_URL
+
+    base_url = urljoin(
+        base_url,
+        "/".join((str(app), str(endpoint)))
+    ) + "/?"
+
+    result = []
+    with requests.Session() as session:
+        url = compose_url(base_url, params)
+        data = attempt_delay(session, url)
+        total_pages = data["pagination"]["total_pages"]
+        result.extend(data["items"])
+        if total_pages == 1: 
+            for i in data['items']:
+                    yield i
+
+        futures = {}
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            # Start the load operations and mark each future with its URL
+            for page in range(2, total_pages + 1):
+                url = compose_url(base_url, params, page)
+                futures[executor.submit(attempt_delay, session,url)] = url
+            for future in concurrent.futures.as_completed(futures):
+                _ = futures[future]
+                resp = future.result()
+                if result: #incorporating the first page
+                    resp["items"].extend(result)
+                    result = []
+                for i in resp['items']:
+                    yield i
