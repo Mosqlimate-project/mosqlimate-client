f586bbfab77e925f5a39e8dc1a6f84275fde73af
Luã Bida Vacaro <luabidaa@gmail.com>
2025-04-01 17:13:41 -0300

    chore(registry): update _prediction_post_impl

--- mosqlient/registry/_prediction_post_impl.py
+++ mosqlient/registry/_prediction_post_impl.py
@@ -3,28 +3,26 @@ __all__ = ["upload_prediction"]
 import requests
 from datetime import date
 
-import pandas as pd
-
-from mosqlient import Client
-from .models import Prediction, Model
+from .models import Prediction
 
 
 def upload_prediction(
+    api_key: str,
     model_id: int,
     description: str,
     commit: str,
     predict_date: str | date,
-    prediction: str | list[dict] | pd.DataFrame,
-    api_key: str,
-    **kwargs
+    adm_1: str,
+    adm_2: int,
+    prediction: list[dict],
 ) -> requests.Response:
-    client = Client(x_uid_key=api_key)
-    model = Model.get(id=model_id)[0]
-    return Prediction(
-        client=client,
-        model=model,
+    return Prediction.post(
+        api_key=api_key,
+        model=model_id,
         description=description,
         commit=commit,
         predict_date=predict_date,
-        data=prediction,
-    ).post(**kwargs)
+        adm_1=adm_1,
+        adm_2=adm_2,
+        prediction=prediction,
+    )
